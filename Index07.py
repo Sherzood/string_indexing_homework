3dbbf2c9b463bb6aebf67f530081579051244b2b
sherzod89 <sherzodms@inbox.ru>
2022-03-27 22:35:58 +0300

    01

--- Index07.py
+++ Index07.py
@@ -6,9 +6,10 @@ def main(s,n):
     Returns:
         str: answer
     """
+    answer='False'
     if len(s)>n:
         return s[n]
     else:
-        return "False"    
+        return answer   
 
 
