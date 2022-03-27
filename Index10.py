21913b686dc2408bf942afa28fde7c0af00728c4
sherzod89 <sherzodms@inbox.ru>
2022-03-27 23:17:02 +0300

    01

--- Index10.py
+++ Index10.py
@@ -6,4 +6,16 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    sd=0
+    if s[0].isdigit():
+        sd+=int(s[0])
+    if s[1].isdigit():
+        sd+=int(s[1])
+    if s[2].isdigit():
+        sd+=int(s[2])
+    if s[3].isdigit():
+        sd+=int(s[3])  
+    if s[4].isdigit():
+        sd+=int(s[4])
+    return sd
+                    
